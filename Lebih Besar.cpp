3408dae35648430828927c8f924d7422fea464e1
ekinwijaya <41121297+ekinwijaya@users.noreply.github.com>
2018-07-12 12:13:54 +0700

    Add files via upload

---
+++ Lebih Besar.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){	
+int a;
+int b;
+int max;
+cout<<"Input A"<<endl;
+cin>>a;
+cout<<"Input B"<<endl;
+cin>>b;
+
+if(a>b){
+	max = a;
+	cout<<"Nilai A Lebih Besar";
+}
+else {
+	max = b;
+	cout<<"Nilai B Lebih Besar";
+}
+
+getch();
+	
+}
